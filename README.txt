9837728ccc8869f32b9372a8230f61026eba7058
u7iD <29367496+u7iD@users.noreply.github.com>
2020-05-04 22:19:17 -0700

    First try at this app

---
+++ README.txt
@@ -0,0 +1,18 @@
+Todo
+functionality
+  - use local storage for lowest-scoring game
+style
+  - animation for flipped a card (to show it and to show the back of the card)
+  - tranforms the game title text "MATCH -A- GIF" 
+
+Additional feature (compared to the requirements or demo app):
+- When starting the game, a user can optionally set the number of unique cards (the app will display twice as many cards). I use localStorage to pass the value for the variable.
+
+What I learned
+- Trying to pass numOfUniqueCards as a global var from index.html to game.html doesn't work
+"You misunderstood what “global variables” are in JavaScript in the browser. They are still tied to the page they were set in, they do not exist in other pages." 
+source: https://stackoverflow.com/questions/27765666/passing-variable-through-javascript-from-one-html-page-to-another-page
+
+- CSS (see style.css)
+ vertically center text in div by using line-height
+ align div with text
---
+++ client.js
@@ -0,0 +1,140 @@
+// image for the back of any card
+var backOfCard = "soap.png";
+
+/**  number of unique cards in the game; value from localStorage
+ */
+var numOfUniqueCards = localStorage.getItem("numOfUniqueCards");
+
+// mini cycle in the game - clicking and showing two cards,
+// then having them either stay shown (when they match) or turned back again
+// num of cards in the cycle; can be 0, 1 or 2
+var numOfCardsShown = 0;
+// total number of cards that have shown - to display in the counter
+var totalCardsShown = 0;
+// in a mini cycle, the first card can be null, or the first card
+var firstCard;
+
+// array that holds all gifs
+var gifs = [];
+for (var i = 0; i < numOfUniqueCards; i++) {
+  gifs.push("gifs/bloom" + i + ".gif");
+}
+// double gifs array size
+gifs = gifs.concat(gifs);
+
+// use the array randomizedIndices to randomize the indices of gifs
+// for example randomizedIndices may be [2, 1, 0, 5, 3, 4] for gifs.length 6
+var randomizedIndices = randomize(0, numOfUniqueCards * 2 - 1);
+
+/** 
+  Create the cards in code
+  <div class="card">
+      <img src="soap.png" id="card0" data-show="false"/>
+  </div>
+*/
+function setupNewGame() {
+  var divAllCards = document.getElementsByClassName("allCards")[0];
+  var divCard, imgCard, divCounter, textCounter;
+  for (var i = 0; i < gifs.length; i++) {
+    divCard = document.createElement("div");
+    imgCard = document.createElement("img");
+    divCard.className = "card";
+    imgCard.className = "cardImg";
+    imgCard.id = "card" + i;
+    imgCard.src = backOfCard;
+    imgCard.setAttribute("data-matched", false);
+    imgCard.addEventListener("click", cardClick);
+    divCard.appendChild(imgCard);
+    divAllCards.appendChild(divCard);
+  }
+  // add the counter in the middle
+  var allCards = document.getElementsByClassName("card");
+  divCounter = document.createElement("div");
+  divCounter.className = "counter";
+  textCounter = document.createTextNode(totalCardsShown);
+  divCounter.appendChild(textCounter);
+  divAllCards.insertBefore(divCounter, allCards[numOfUniqueCards]);
+}
+
+/**
+ * This function takes min max values and return the integers between them (including min and max)
+ * in random orders
+ * For example for the arguments (0, 5), return value could be [3, 0, 1, 5, 4, 2];
+ * @param {*} minVal
+ * @param {*} maxVal
+ */
+function randomize(minVal, maxVal) {
+  var numOfUniqueVals = maxVal - minVal + 1;
+  // obj has Math.random as key, integers as values
+  var obj = {};
+  var arrRandomized = (sortedKeys = []);
+  for (var i = 0; i < numOfUniqueVals; i++) {
+    obj[Math.random()] = i;
+  }
+  // sort the keys
+  sortKeys = Object.keys(obj).sort();
+  // return the values in an array
+  for (var key of sortKeys) {
+    arrRandomized.push(obj[key]);
+  }
+  return arrRandomized;
+}
+
+function cardClick(event) {
+  var currentCard = event.target;
+  var divCounter = document.getElementsByClassName("counter")[0];
+  // if there are two cards shown already, clicking does nothing
+  if (numOfCardsShown >= 2) {
+    return;
+  }
+  // clicking on a card that's already got a match
+  // does nothing
+  if (currentCard.getAttribute("data-matched") === "true") {
+    return;
+  }
+
+  // clicking on the same card twice does nothing
+  if (firstCard && currentCard.id === firstCard.id) {
+    return;
+  }
+
+  // show card and increment counter
+  showCard(currentCard);
+  numOfCardsShown++;
+  divCounter.textContent = ++totalCardsShown;
+
+  if (numOfCardsShown === 1) {
+    firstCard = currentCard;
+  } else if (numOfCardsShown === 2) {
+    if (isAMatch(currentCard, firstCard)) {
+      currentCard.setAttribute("data-matched", true);
+      firstCard.setAttribute("data-matched", true);
+    } else {
+      numOfCardsShown = 2;
+      // show the current card for at least second
+      setTimeout(turnCardBack, 1000, currentCard);
+      // otherwise if I code like this the current card doesn't appear
+      // turnCardBack(currentCard);
+      setTimeout(turnCardBack, 1000, firstCard);
+    }
+    firstCard = null;
+    numOfCardsShown = 0;
+  }
+}
+
+function isAMatch(nodeImg1, nodeImg2) {
+  return nodeImg1.src === nodeImg2.src;
+}
+
+// gifs var is a fixed array with gifs;
+// randomizedIndices is an array that has the array indices in
+// random order
+// each card id has values "card0", "card1", "card2" etc.
+function showCard(nodeImg) {
+  var cardNum = +nodeImg.id.slice(4);
+  nodeImg.src = gifs[randomizedIndices[cardNum]];
+}
+
+function turnCardBack(nodeImg) {
+  nodeImg.src = backOfCard;
+}
